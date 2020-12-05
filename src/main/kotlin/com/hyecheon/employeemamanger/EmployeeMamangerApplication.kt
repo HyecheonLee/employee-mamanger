d466c55f2fc42b52e7627399162bf95cae3e9065
hyecheon <rainbow880616@gmail.com>
2020-12-05 16:51:24 +0900

    Initial commit

---
+++ src/main/kotlin/com/hyecheon/employeemamanger/EmployeeMamangerApplication.kt
@@ -0,0 +1,11 @@
+package com.hyecheon.employeemamanger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmployeeMamangerApplication
+
+fun main(args: Array<String>) {
+	runApplication<EmployeeMamangerApplication>(*args)
+}
---
+++ src/test/kotlin/com/hyecheon/employeemamanger/EmployeeMamangerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyecheon.employeemamanger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmployeeMamangerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
